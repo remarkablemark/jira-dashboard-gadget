f2861ef8a511afe07d30ad34d82f9ed927cb9f68
Mark <mark@remarkablemark.org>
2024-03-03 20:58:57 -0500

    refactor: move to workspace

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-import Resolver from '@forge/resolver';
-
-const resolver = new Resolver();
-
-resolver.define('getText', (request) => {
-  // eslint-disable-next-line no-console
-  console.log(request);
-
-  return 'Hello world!';
-});
-
-export const handler = resolver.getDefinitions();
+export { handler } from './resolvers';
---
+++ src/resolvers/index.js
@@ -0,0 +1,12 @@
+import Resolver from '@forge/resolver';
+
+const resolver = new Resolver();
+
+resolver.define('getText', (request) => {
+  // eslint-disable-next-line no-console
+  console.log(request);
+
+  return 'Hello world!';
+});
+
+export const handler = resolver.getDefinitions();
