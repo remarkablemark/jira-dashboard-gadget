0ebfc4287dc11d50c099e601ef029919c316f64d
Mark <mark@remarkablemark.org>
2024-03-04 14:05:05 -0500

    build(frontend): fix alias in vite.config.mjs

--- src/frontend/vite.config.mjs
+++ src/frontend/vite.config.mjs
@@ -1,7 +1,13 @@
 import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: './',
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: resolve(__dirname, 'src'),
+    },
+  },
 });
