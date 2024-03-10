d572e38a195166f23b55d22e9135a752a84059d3
Mark <mark@remarkablemark.org>
2024-03-10 15:37:08 -0400

    refactor(frontend): optimize log helpers

--- src/frontend/src/helpers/log.ts
+++ src/frontend/src/helpers/log.ts
@@ -1,8 +1,12 @@
 /* eslint-disable no-console */
 const NAME = `[${import.meta.env.VITE_APP_NAME}@${import.meta.env.VITE_APP_VERSION}]`;
 
+const error = console.error.bind(null, NAME);
+const info = console.info.bind(null, NAME);
+const warn = console.warn.bind(null, NAME);
+
 export const log = {
-  error: console.error.bind(null, NAME),
-  info: console.info.bind(null, NAME),
-  warn: console.warn.bind(null, NAME),
+  error,
+  info,
+  warn,
 };
