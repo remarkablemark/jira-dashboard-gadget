d6f08851dfd43a956fc8f59ad03af62f09018d2b
Mark <mark@remarkablemark.org>
2024-11-23 15:44:50 -0500

    test(frontend): update App.test.tsx

--- src/frontend/src/app/App.test.tsx
+++ src/frontend/src/app/App.test.tsx
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import { view } from '@forge/bridge';
+import { render, screen, waitFor } from '@testing-library/react';
 
+import type { FullContext } from '../types';
 import App from './App';
 
-it('renders loading icon', async () => {
+jest.mock('../edit', () => () => <>edit</>);
+jest.mock('../view', () => () => <>view</>);
+
+const mockedView = jest.mocked(view);
+
+it('renders loading icon', () => {
   render(<App />);
   expect(screen.getByLabelText('Loading')).toBeInTheDocument();
+});
+
+it.each(['edit', 'view'])('renders %s', async (entryPoint) => {
+  mockedView.getContext.mockResolvedValueOnce({
+    extension: { entryPoint },
+  } as unknown as FullContext);
+  render(<App />);
+  await waitFor(() => {
+    expect(screen.getByText(entryPoint)).toBeInTheDocument();
+  });
 });
