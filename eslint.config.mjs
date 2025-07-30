ecb24001c9e36480a59f90436577517a9276af53
Mark <mark@remarkablemark.org>
2025-07-29 21:25:26 -0400

    chore(eslint): tidy eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,55 +1,36 @@
-import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import { fixupPluginRules, includeIgnoreFile } from '@eslint/compat';
-import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
 import prettier from 'eslint-plugin-prettier';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const gitignorePath = path.resolve(__dirname, '.gitignore');
+const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url));
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
-
-/** @type {import('eslint').Linter.FlatConfig} */
-export default [
+export default tseslint.config(
   includeIgnoreFile(gitignorePath),
 
-  {
-    ignores: ['dist/'],
-  },
-
-  ...compat.extends(
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-  ),
+  { ignores: ['dist/'] },
 
   {
     plugins: {
-      '@typescript-eslint': typescriptEslint,
       'react-hooks': fixupPluginRules(reactHooks),
       'simple-import-sort': simpleImportSort,
       prettier,
       react,
     },
 
+    extends: [js.configs.recommended],
+
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
       },
-      parser: tsParser,
     },
 
     rules: {
@@ -63,4 +44,6 @@ export default [
       'react-hooks/exhaustive-deps': 'error',
     },
   },
-];
+
+  tseslint.configs.recommended,
+);
